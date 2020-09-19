506cd2bdecbd319f0bd7097d581339c1be80dbb5
Matteo <mmanighe@lix.polytechnique.fr>
2020-09-19 20:37:33 +0200

    Small fix, still incomplete

--- focus_coq/lkf.v
+++ focus_coq/lkf.v
@@ -1,4 +1,14 @@
 (* Problems of classical focalization in Coq *)
-
+From elpi Require Import elpi.
 (* What was underlined by Pichon *)
-Goal forall a:Prop, (fix := (a -> False)) -> a.
+Parameter a:Prop.
+Inductive weird : Prop :=
+  mmh:  (a -> False) -> weird.
+
+Goal weird -> a.
+
+Elpi Command test.
+Elpi Query lp:{{
+  coq.locate "bubu" (indt X),
+  coq.env.indt X _ _ _ Type Kn Clauses.
+}}.
